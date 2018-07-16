64da6061eb3b58ab3019c50f38acd82d9c6d07bb
kasparm <10716532+kasparm@users.noreply.github.com>
2018-07-16 16:03:45 -0700

    Adapted code to code convention.

--- extract/extractdata.py
+++ extract/extractdata.py
@@ -6,62 +6,64 @@ import matlab.engine
 eng = matlab.engine.start_matlab()
 
 
-def extractData(caseName, locRead, nStart, nEnd):
+def extract_data(case_name, data_location, start_index, end_index):
     """Takes subintervals from simulation in matlab
     binary formate, converts and connects it into csv format.
     It uses the matlab functions get_power_output_from_gen
     and get_load_on_branch to extract data.
-    
+
     WARNING:
-    dataRange is hard coded
+    date_range is hard coded
 
     """
-    
+
     start = time.process_time()
-    for i in range(nStart, nEnd+1):
+    for i in range(start_index, end_index+1):
         print('Reading'+str(i))
-        filename = '/sub_result_' + str(i)
+        filename = case_name + '_sub_result_' + str(i)
         # Call matlab functions
-        matlabPG = eng.get_power_output_from_gen(locRead + filename)
-        matlabPF = eng.get_load_on_branch(locRead + filename)
-        if i > nStart:
-            PG = PG.append(pd.DataFrame(np.array(matlabPG._data).reshape(
-                matlabPG.size[::-1])))
-            PF = PF.append(pd.DataFrame(np.array(matlabPF._data).reshape(
-                matlabPF.size[::-1])))
+        matlab_pg = eng.get_power_output_from_gen(data_location + filename)
+        matlab_pf = eng.get_load_on_branch(data_location + filename)
+        if i > start_index:
+            pg = pg.append(pd.DataFrame(np.array(matlab_pg._data).reshape(
+                matlab_pg.size[::-1])))
+            pf = pf.append(pd.DataFrame(np.array(matlab_pf._data).reshape(
+                matlab_pf.size[::-1])))
         else:
-            PG = pd.DataFrame(np.array(matlabPG._data).reshape(
-                matlabPG.size[::-1]))
-            PG.name = caseName + 'PG'
-            PF = pd.DataFrame(np.array(matlabPF._data).reshape(
-                matlabPF.size[::-1]))
-            PF.name = caseName + 'PF'
+            pg = pd.DataFrame(np.array(matlab_pg._data).reshape(
+                matlab_pg.size[::-1]))
+            pg.name = case_name + 'PG'
+            pf = pd.DataFrame(np.array(matlabpf._data).reshape(
+                matlab_pf.size[::-1]))
+            pf.name = case_name + 'PF'
     end = time.process_time()
-    print('Reading time ' + str(100 * (end-start)) + 's')    
+    print('Reading time ' + str(100 * (end-start)) + 's')
 
     # Set data range
-    dataStart = pd.Timestamp('2010-01-01')
-    dataEnd = pd.Timestamp('2012-12-31 23:00:00')
-    dataRange = pd.dateRange(dataStart, dataEnd, freq='H')
+    date_start = pd.Timestamp('2010-01-01')
+    date_end = pd.Timestamp('2012-12-31 23:00:00')
+    date_range = pd.date_range(date_start, date_end, freq='H')
 
-    PF.index = dataRange[nStart-1:nEnd-1]
-    PG.index = dataRange[nStart-1:nEnd-1]
+    pf.index = date_range[(start_index-1)*4*24:(end_index)*4*24]
+    pg.index = date_range[(start_index-1)*4*24:(end_index)*4*24]
 
     # Shift index of PG becasue bus index in matlab
-    PG = PG.rename(columns=lambda x: x+1)
+    pg = pg.rename(columns=lambda x: x+1)
 
-    return (PG, PF)
+    return (pg, pf)
 
 
-def extractDataAndSave(caseName, locRead, locSave, nStart, nEnd):
+def extract_data_and_save(case_name, data_location, save_location,
+                          start_index, end_index):
     """Extract data and save as csv in locSave locaton."""
-    
-    (PG, PF) = extractData(caseName, locRead, nStart, nEnd)
-    
-    PG.to_csv(locSave+caseName+'PG.csv')
-    PF.to_csv(locSave+caseName+'PF.csv')
+
+    (pg, pf) = extract_data(case_name, data_location, start_index, end_index)
+
+    pg.to_csv(save_location+case_name+'PG.csv')
+    pf.to_csv(save_location+case_name+'PF.csv')
 
 
 if __name__ == "__main__":
     import sys
-    extractDataAndSave(sys.argv[1], sys.argv[2], sys.argv[3], int(sys.argv[4]), int(sys.argv[5]))
+    extract_data_and_save(sys.argv[1], sys.argv[2], sys.argv[3], int(
+        sys.argv[4]), int(sys.argv[5]))
