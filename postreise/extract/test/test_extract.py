64e0f205359f66609186950bbb056f5f3201fc47
kasparm <10716532+kasparm@users.noreply.github.com>
2018-10-10 13:53:52 -0700

    test: Added simple test

---
+++ postreise/extract/test/test_extract.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import unittest
+
+
+sys.path.insert(0,
+                os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import extract_data
+
+
+def test():
+    extract_data.extract_scenario('western_scenarioUnitTest02')
