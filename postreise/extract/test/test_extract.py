029fd035749de9cf4313840cf19dee88c5c95f31
Ben Rouillé d'Orfeuil <brdo@intven.com>
2018-10-17 15:57:52 -0700

    style: Format path

--- postreise/extract/test/test_extract.py
+++ postreise/extract/test/test_extract.py
@@ -2,12 +2,9 @@ import os
 import sys
 import unittest
 
-
-sys.path.insert(0,
-                os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import extract_data
-
 
 def test():
     extract_data.extract_scenario('western_scenarioUnitTest02')
