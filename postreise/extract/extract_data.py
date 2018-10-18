5572c72e11b8ad94312b1e647a4769520f7f9391
Ben Rouillé d'Orfeuil <brdo@intven.com>
2018-10-18 10:29:25 -0700

    feat: Remove function extract_data

--- postreise/extract/extract_data.py
+++ postreise/extract/extract_data.py
@@ -7,57 +7,6 @@ import pandas as pd
 from pandas.core.indexes.datetimes import date_range
 
 
-def extract_data(scenario_name, data_location, start_index, end_index,
-                 date_range):
-    """Takes subintervals from simulation in MATLAB binary format, converts \ 
-        and connects it into csv format. It uses the MATLAB functions \ 
-        get_power_output_from_gen and get_load_on_branch to extract data.
-
-
-    :param str scenario_name: Name of scenario.
-    :param str data_location: Location of data to be extracted.
-    :param int start_index: Start index of the file to be extracted.
-    :param int end_index: End index of the file to be extracted.
-    :param date_range: Pandas date_range object
-    :return: pg and pf pandas dataframe.
-    """
-    # Start MATLAB engine
-    print("Starting MATLAB")
-    eng = matlab.engine.start_matlab()
-    this_dirname = os.path.dirname(__file__)
-    eng.addpath(this_dirname)
-
-    start = time.process_time()
-    for i in range(start_index, end_index+1):
-        print('Reading'+str(i))
-        filename = scenario_name + '_sub_result_' + str(i)
-        # Call MATLAB functions
-        matlab_pg = eng.get_power_output_from_gen(data_location + filename)
-        matlab_pf = eng.get_load_on_branch(data_location + filename)
-        if i > start_index:
-            pg = pg.append(pd.DataFrame(np.array(matlab_pg._data).reshape(
-                matlab_pg.size[::-1])))
-            pf = pf.append(pd.DataFrame(np.array(matlab_pf._data).reshape(
-                matlab_pf.size[::-1])))
-        else:
-            pg = pd.DataFrame(np.array(matlab_pg._data).reshape(
-                matlab_pg.size[::-1]))
-            pg.name = scenario_name + 'PG'
-            pf = pd.DataFrame(np.array(matlab_pf._data).reshape(
-                matlab_pf.size[::-1]))
-            pf.name = scenario_name + 'PF'
-    end = time.process_time()
-    print('Reading time ' + str(100 * (end-start)) + 's')
-
-    eng.quit()
-    print("MATLAB terminated")
-
-    pf.index = date_range[(start_index-1):(end_index)]
-    pg.index = date_range[(start_index-1):(end_index)]
-
-    return (pg, pf)
-
-
 def extract_data_batch(scenario_name, data_location, start_index, end_index,
                        date_range):
     """This function extracts the data in batches using the MATLAB function \ 
@@ -68,7 +17,7 @@ def extract_data_batch(scenario_name, data_location, start_index, end_index,
     :param str data_location: Location of data to be extracted.
     :param int start_index: Start index of the file to be extracted.
     :param int end_index: End index of the file to be extracted.
-    :param date_range: Pandas date_range object
+    :param date_range: pandas date_range object.
     :return: pg and pf pandas dataframe.
     """
 
@@ -103,11 +52,9 @@ def extract_data_and_save(scenario_name, data_location, save_location,
         stored.
     :param int start_index: Start index of the file to be extracted.
     :param int end_index: End index of the file to be extracted.
-    :param date_range: Pandas date_range object
+    :param date_range: Pandas date_range object.
     """
 
-    # (pg, pf) = extract_data(scenario_name, data_location, start_index,
-    #                         end_index, date_range)
     (pg, pf) = extract_data_batch(scenario_name, data_location, start_index,
                                   end_index, date_range)
 
@@ -116,7 +63,7 @@ def extract_data_and_save(scenario_name, data_location, save_location,
 
 
 def extract_scenario(scenario_name):
-    """Extract data given scenario_name. Lookup data from ScenarioList.csv
+    """Extract data given scenario_name. Lookup data from ScenarioList.csv.
 
     :param str scenario_name: Name of scenario to be extracted.
     """
@@ -132,7 +79,7 @@ def extract_scenario(scenario_name):
         print('No scenario with name ' + scenario_name)
         return
     if scenario.extract.values[0]:
-        print('Scenario already extracted or does not want to be extracted.')
+        print('Scenario already extracted or does not want to be extracted')
         return
     # Set data range
     date_start = pd.Timestamp(scenario.start_date.values[0])
