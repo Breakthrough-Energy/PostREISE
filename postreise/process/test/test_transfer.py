c7ad9e7016ba6ec872fd5d6084b26dc38971bffa
kasparm <10716532+kasparm@users.noreply.github.com>
2018-11-05 09:50:04 -0800

    feat: Added test for transfer class

---
+++ postreise/process/test/test_transfer.py
@@ -0,0 +1,21 @@
+import pytest
+
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import transferdata as td
+
+def test_setup_server_connection():
+    transfer = td.TransferData()
+    transfer._setup_server_connection()
+
+def test_get_scenario_file_from_server():
+    transfer = td.TransferData()
+    sftp = transfer._setup_server_connection()
+    transfer._get_scenario_file_from_server(sftp)
+    
+def test_get_data():
+    od = td.OutputData()
+    od.get_data('western_scenarioUnitTest02','PG')
+    od.get_data('western_scenarioUnitTest02','PF')
