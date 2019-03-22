9bb46f63717fd9cf990f4d2ccee2f2d9865918a1
Ben RdO <brdo@intven.com>
2019-03-22 11:03:17 -0700

    feat: Add global variables for data transfer

--- postreise/process/const.py
+++ postreise/process/const.py
@@ -1,2 +1,6 @@
 SERVER_ADDRESS = 'zeus.intvenlab.com'
 SCENARIO_LIST_LOCATION = '/home/EGM/v2/ScenarioList.csv'
+REMOTE_DIR_BASE = "/home/EGM/v2/raw"
+REMOTE_DIR_INPUT = "/home/EGM/v2/data/input"
+REMOTE_DIR_OUTPUT = "/home/EGM/v2/data/output"
+LOCAL_DIR = None
