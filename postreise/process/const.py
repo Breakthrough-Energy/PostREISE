e219c82c981f54da117a9bccf5d78a62702cecd2
Ben RdO <brdo@intven.com>
2019-04-09 16:15:15 -0700

    feat: Define EXECUTE_LIST_LOCATION

--- postreise/process/const.py
+++ postreise/process/const.py
@@ -1,6 +1,7 @@
 SERVER_ADDRESS = 'zeus.intvenlab.com'
 SCENARIO_LIST_LOCATION = '/home/EGM/v2/ScenarioList.csv'
-REMOTE_DIR_BASE = "/home/EGM/v2/raw"
-REMOTE_DIR_INPUT = "/home/EGM/v2/data/input"
-REMOTE_DIR_OUTPUT = "/home/EGM/v2/data/output"
+EXECUTE_LIST_LOCATION = '/home/EGM/v2/ExecuteList.csv'
+REMOTE_DIR_BASE = '/home/EGM/v2/raw'
+REMOTE_DIR_INPUT = '/home/EGM/v2/data/input'
+REMOTE_DIR_OUTPUT = '/home/EGM/v2/data/output'
 LOCAL_DIR = None
