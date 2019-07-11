a633610ff9c9bb9f508004f5ad54820f81efcd08
Ben RdO <brdo@intven.com>
2019-07-11 15:10:25 -0700

    fix: Use absolute path to load files

--- postreise/analyze/test/test_transmission.py
+++ postreise/analyze/test/test_transmission.py
@@ -1,24 +1,28 @@
-import pandas as pd
-
 from postreise.analyze import transmission
+
+import pandas as pd
+import os
 
 
 def test_transmission():
     """Tests transmission.py by checking:
-    
+
     1. all p-values are in [0,1].
     2. all line distances are shorter than the longest line in the US.
 
     .. note::
-    To run faster, shortened versions of the branch and normalized power \ 
+    To run faster, shortened versions of the branch and normalized power \
     flow files are used.
     """
 
-    data_dir = 'data/'
+    data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
 
-    branches = pd.read_csv(data_dir + 'branches_for_testing_100.csv')
-    cong_base = pd.read_csv(data_dir + 'congestion_base_for_testing_1000.csv',
-                            index_col='Unnamed: 0')
+    branches = pd.read_csv(
+        os.path.join(data_dir, 'branches_for_testing_100.csv'))
+    cong_base = pd.read_csv(os.path.join(
+        data_dir, 'congestion_base_for_testing_1000.csv'),
+        index_col='Unnamed: 0')
+
     cong_base.index = pd.to_datetime(cong_base.index)
     branches.index = cong_base.columns
 
