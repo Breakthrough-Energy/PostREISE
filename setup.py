012afdab6e26bc7699da7764feb3751f7749e15c
kasparm <10716532+kasparm@users.noreply.github.com>
2019-07-18 16:33:39 -0700

    chore: Update version number to 0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='postreise',
-      version='0.2',
+      version='0.3',
       description='Extract, analyze and plot data from scenario',
       url='https://github.com/intvenlab/PostREISE',
       author='Kaspar Mueller',
