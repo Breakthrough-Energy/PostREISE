562837ae24e1a05fd4fbdf0e86253ff6d12ced28
Daniel Olsen <dolsen@intven.com>
2019-09-24 18:09:13 -0700

    fix: correcting references to grid object in carbon analysis

--- postreise/analyze/carbon.py
+++ postreise/analyze/carbon.py
@@ -1,3 +1,4 @@
+import numpy as np
 from numpy.polynomial.polynomial import polyval
 import pandas as pd
 
@@ -12,21 +13,21 @@ def generate_carbon_stats(pg, grid, name=None):
     
     costs = calc_costs(pg, grid.gencost)
     heat = np.zeros_like(costs)
-    carbon = pd.DataFrame().reindex_like(pg)
+    carbon = pd.DataFrame(np.zeros_like(pg), index=pg.index, columns=pg.columns)
     
-    coal_indices = (grid.genfuel == 'coal')
-    ng_indices = (grid.genfuel == 'ng')
+    coal_indices = (grid.plant['type'] == 'coal')
+    ng_indices = (grid.plant['type'] == 'ng')
     # Can't do dfo yet because input data says zero fuel cost
 
     heat[:, coal_indices] = (
-        costs[:, coal_indices] / grid.gen['GenFuelCost'].values[coal_indices])
+        costs[:, coal_indices] / grid.plant['GenFuelCost'].values[coal_indices])
     heat[:, ng_indices] = (
-        costs[:, ng_indices] / grid.gen['GenFuelCost'].values[ng_indices])
+        costs[:, ng_indices] / grid.plant['GenFuelCost'].values[ng_indices])
 
     # MMBTu of fuel to metric tons of CO2
     # Source: https://www.epa.gov/energy/greenhouse-gases-equivalencies-calculator-calculations-and-references
-    carbon.iloc[:, coal_indices] = heat[:, coal_indices] * 26.05 * 44/12 / 1000
-    carbon.iloc[:, ng_indices] = heat[:, coal_indices] * 14.46 * 44/12 / 1000
+    carbon.loc[:, coal_indices] = heat[:, coal_indices] * 26.05 * 44/12 / 1000
+    carbon.loc[:, ng_indices] = heat[:, ng_indices] * 14.46 * 44/12 / 1000
     
     return carbon
     
@@ -45,8 +46,8 @@ def calc_costs(pg, gencost):
     cost_type = gencost['type']
     if not cost_type.where(cost_type == 2).equals(cost_type):
         raise ValueError('Not all polynomials!')
-    # get polynomials in columns, discarding non-coeff data
-    coefs = gencost.values.T[:-4:-1,:]
+    # get ordered polynomial coefficients in columns, discarding non-coeff data
+    coefs = gencost.values.T[-2:3:-1,:]
     # elementwise, evaluate polynomial where x = value
     costs = polyval(pg.values, coefs, tensor=False)
     
