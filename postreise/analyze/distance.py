080a3f16548a73e8b0a190c143c8f157c151fde9
Ben RdO <brdo@intven.com>
2019-11-01 14:48:45 -0700

    refactor: Rewrite functions to keep passed data frame unmodified

--- postreise/analyze/distance.py
+++ postreise/analyze/distance.py
@@ -4,8 +4,8 @@ import geopy.distance
 def great_circle_distance(x):
     """Calculates distance between two sites.
 
-    :param pandas data frame of branches, which has lat lon fields
-    :return: (*float*) -- distance between two sites (in km.).
+    :param pandas.dataFrame x: start and end point corrdinates of branches.
+    :return: (*float*) -- length of line (in km.).
     """
     site_coords = (x.from_lat, x.from_lon)
     place2_coords = (x.to_lat, x.to_lon)
