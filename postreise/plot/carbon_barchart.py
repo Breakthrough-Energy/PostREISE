6a42d5592cff4fb75821c48b8152f863b6ba5cc2
Vicky Hunt <49412554+victoriahunt@users.noreply.github.com>
2019-11-01 10:54:29 -0700

    feat: Create demo notebook to show carbon plotting

--- postreise/plot/carbon_barchart.py
+++ postreise/plot/carbon_barchart.py
@@ -2,42 +2,53 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 def carbon_barchart(dict1, dict2, name1, name2):
+    """Makes barchart of carbon emissions by fuel type for two scenarios.
+    :param dict dict1: carbon emissions by type and bus, scenario 1
+    :param dict dict2: carbon emissions by type and bus, scenario 2
+    :param str name1: name of scenario 1, for labeling plot
+    :param str name1: name of scenario 2, for labeling plot
+    :return: (*matplotlib plt*) -- barplot of carbon emissions
+    """
     plt.rcParams.update({'font.size': 22})
-    fig = plt.figure(figsize=(7,2))
-    objects = ( name1, name2)
+    fig = plt.figure(figsize=(7, 2))
+    objects = (name1, name2)
     y_pos = np.arange(len(objects))
     carbon_val = [sum(dict1['coal'].values()),
-               sum(dict2['coal'].values())]
+                  sum(dict2['coal'].values())]
 
-    plt.barh(y_pos, carbon_val, align='center', alpha=0.25, color = ['black'])
+    plt.barh(y_pos, carbon_val, align='center', alpha=0.25, color=['black'])
 
     plt.yticks(y_pos, objects)
     plt.xticks(np.arange(0, 2.1e8, .5e8))
     plt.xlabel('Tons')
     plt.title('Coal: C$O_2$  Emissions', y=1.04)
-    #plt.tight_layout()
 
     plt.show()
 
-    fig = plt.figure(figsize=(7,2))
+    fig = plt.figure(figsize=(7, 2))
     objects = ('2030 all match CA', '2016 base')
     y_pos = np.arange(len(objects))
     carbon_val = [sum(dict1['ng'].values()),
-               sum(dict2['ng'].values())]
+                  sum(dict2['ng'].values())]
 
-    plt.barh(y_pos, carbon_val, align='center', alpha=0.25, color = ['purple'])
+    plt.barh(y_pos, carbon_val, align='center', alpha=0.25, color=['purple'])
 
     plt.yticks(y_pos, objects)
     plt.xticks(np.arange(0, 2.1e8, .5e8))
     plt.xlabel('Tons')
     plt.title('Natural Gas: C$O_2$ Emissions', y=1.04)
-    #plt.tight_layout()
 
     plt.show()
     return
-    
+
+
 def carbon_percentdiff(dict1, dict2):
+    """Cal percentage change in carbon emissions of two scenarios.
+    :param dict dict1: carbon emissions by type and bus, scenario 1
+    :param dict dict2: carbon emissions by type and bus, scenario 2
+    :return float: percentage difference in carbon emissions
+    """
     sum1 = sum(dict1['coal'].values()) + sum(dict1['ng'].values())
     sum2 = sum(dict2['coal'].values()) + sum(dict2['ng'].values())
-    print(round(100*(1- sum2/sum1),2))
+    print(round(100*(1 - sum2/sum1), 2))
     return
--- postreise/plot/carbon_plot.py
+++ postreise/plot/carbon_plot.py
@@ -1,8 +1,8 @@
-#import general functions
+# import general functions
 import pandas as pd
 import numpy as np
 
-#import mapping functions
+# import mapping functions
 from bokeh.plotting import show, figure
 from pyproj import Proj, transform
 from bokeh.tile_providers import get_provider, Vendors
@@ -11,109 +11,131 @@ from bokeh.models import ColumnDataSource, LabelSet
 from matplotlib import pyplot as plt
 from bokeh.layouts import row
 
-def makemap_carbon(bus_map, scenario_name, color1 = 'black', 
-                   color2 = 'purple', label1 = "Coal: tons", 
-                   label2 = "NG: tons"):
-    """Make map showing carbon emissions, color coded by fuel type, size indicates emissions (area)
-    :param: grid bus object with coordinates for bus, df
-    :param carbon_dict: coal and ng emissions by bus and type dictionary
-    :return: map
+
+def makemap_carbon(bus_map, scenario_name, color1='black',
+                   color2='purple', label1="Coal: tons",
+                   label2="NG: tons"):
+    """Make map of carbon emissions, color code by fuel type, size/area indicates emissions
+    :param pandas.DataFrame bus_map: emissions and coordinates of buses
+    :param str scenario_name: name of scenario for labeling
+    :param str color1: color assigned for coal, default to black
+    :param str color2: color assigned for ng, default to purple
+    :param str label1: label for legend associated with coal
+    :param str label2: label for legend associated with ng
+    :return bokeh.figure: map of carbon emissions per bus color code by type
     """
     bus_source = ColumnDataSource({
         'x': bus_map['x'],
         'y': bus_map['y'],
         'coal': (bus_map['coal']/500)**0.5,
         'ng': (bus_map['ng']/500)**0.5})
-    
+
     # Set up figure
     tools: str = "pan,wheel_zoom,reset,hover,save"
 
-    p = figure(title=scenario_name, tools=tools, x_axis_location=None, 
+    p = figure(title=scenario_name, tools=tools, x_axis_location=None,
                y_axis_location=None, plot_width=800, plot_height=800)
-    
-    p_legend = figure(  x_axis_location=None, 
+
+    p_legend = figure(x_axis_location=None,
                       y_axis_location=None, toolbar_location=None,
-                      plot_width=200, plot_height=400, 
-                     y_range=(0, 4),
-                      x_range=( 0, 2))
+                      plot_width=200, plot_height=400,
+                      y_range=(0, 4),
+                      x_range=(0, 2))
 
     p.add_tile(get_provider(Vendors.CARTODBPOSITRON_RETINA))
 
-    p.circle('x','y', fill_color=color1, 
+    p.circle('x', 'y', fill_color=color1,
              color=color1,
              alpha=0.25, size='coal',
-             source = bus_source)
-    
-    p.circle('x','y',
-             fill_color=color2, 
+             source=bus_source)
+
+    p.circle('x', 'y',
+             fill_color=color2,
              color=color2,
-             alpha=0.25, 
+             alpha=0.25,
              size='ng',
-             source = bus_source)
+             source=bus_source)
 
-    #legend is custom: white square background and circles of different size on top
-    
-    p_legend.square(1, [1, 3],
-             fill_color='white', color='white', 
-             size=300)
-    
-    p_legend.square(1, [1, 3],
-             fill_color='white', color='black', 
-             size=(2000000/100)**0.5)
+    # legend is custom: white square background and circles of different size on top
 
-    
-    p_legend.circle(1, y = np.repeat([1,3],3),
-             fill_color = np.repeat([color1, color2],3), 
-             color = np.repeat([color1, color2],3), alpha = 0.25, 
-             size = [(5000000/500)**0.5, (2000000/500)**0.5, (100000/500)**0.5]*2)
- 
+    p_legend.square(1, [1, 3],
+                    fill_color='white', color='white',
+                    size=300)
+
+    p_legend.square(1, [1, 3],
+                    fill_color='white', color='black',
+                    size=(2000000/100)**0.5)
+
+    p_legend.circle(1, y=np.repeat([1, 3], 3),
+                    fill_color=np.repeat([color1, color2], 3),
+                    color=np.repeat([color1, color2], 3), alpha=0.25,
+                    size=[(5000000/500)**0.5, (2000000/500)**0.5, (100000/500)**0.5]*2)
+
     source = ColumnDataSource(
-        data=dict(x=[1,1,1,0.5,1,1,1,0.5],
-                  y= [.9, 1.1, 1.3, 1.55, 2.9,3.1, 3.3, 3.55],
-                  text=['100000', '2000000','5000000', 
-                        label1, '100000', '2000000','5000000', label2]))   
-    
+        data=dict(x=[1, 1, 1, 0.5, 1, 1, 1, 0.5],
+                  y=[.9, 1.1, 1.3, 1.55, 2.9, 3.1, 3.3, 3.55],
+                  text=['100000', '2000000', '5000000',
+                        label1, '100000', '2000000', '5000000', label2]))
+
     source1 = ColumnDataSource(
         data=dict(x=[10]*8,
-                  y= [10]*8,
-                  text=['100000', '2000000','5000000', 
-                        label1, '100000', '2000000','5000000', label2]))   
-    
-    labels = LabelSet(x = 'x', y = 'y',
-                      text = 'text',source = source, level = 'glyph', 
+                  y=[10]*8,
+                  text=['100000', '2000000', '5000000',
+                        label1, '100000', '2000000', '5000000', label2]))
+
+    labels = LabelSet(x='x', y='y',
+                      text='text', source=source, level='glyph',
                       x_offset=0, y_offset=0, render_mode='css')
 
-    
     p_legend.add_layout(labels)
 
-    #p.add_layout(legend)
     output_notebook()
     show(row(p_legend, p))
 
-    return 
-    
-def makemap_carbon_compare(bus_map1, bus_map2, scenario_name1, scenario_name2):
-    makemap_carbon(bus_map1,scenario_name1)
-    makemap_carbon(bus_map2,scenario_name2)
+    return
 
-    bus_map_merge = bus_map1.merge(bus_map2, right_index = True, left_index = True, how = 'outer' )
+
+def makemap_carbon_compare(bus_map1, bus_map2, scenario_name1, scenario_name2):
+    """Make map of carbon emissions, color code by fuel type, size/area indicates emissions
+    :param pandas.DataFrame bus_map1: emissions and coordinates of buses, scenario1
+    :param pandas.DataFrame bus_map2: emissions and coordinates of buses, scenario2
+    :param str scenario_name2: name of scenario 1 for labeling
+    :param str scenario_name2: name of scenario 2 for labeling
+    :return bokeh.figure: figures for each scenario carbon emissions plus a coomparison map 
+        indicating increase or decrease
+    """
+    makemap_carbon(bus_map1, scenario_name1)
+    makemap_carbon(bus_map2, scenario_name2)
+
+    bus_map_merge = bus_map1.merge(
+        bus_map2, right_index=True, left_index=True, how='outer')
     bus_map_merge.coal_x = bus_map_merge.coal_x.fillna(0)
     bus_map_merge.coal_y = bus_map_merge.coal_y.fillna(0)
     bus_map_merge.ng_x = bus_map_merge.ng_x.fillna(0)
     bus_map_merge.ng_y = bus_map_merge.ng_y.fillna(0)
 
-    bus_map_merge['coal_dif'] = bus_map_merge['coal_x'] - bus_map_merge['coal_y']
+    bus_map_merge['coal_dif'] = bus_map_merge['coal_x'] - \
+        bus_map_merge['coal_y']
     bus_map_merge['ng_dif'] = bus_map_merge['ng_x'] - bus_map_merge['ng_y']
     bus_map_merge['x'] = bus_map_merge['x_x'].fillna(bus_map_merge['x_y'])
     bus_map_merge['y'] = bus_map_merge['y_x'].fillna(bus_map_merge['y_y'])
     bus_map_merge["coal"] = 0
     bus_map_merge["ng"] = 0
-    bus_map_merge.loc[bus_map_merge.coal_dif >0, ['coal']] = bus_map_merge['coal_dif']; bus_map_merge
-    bus_map_merge.loc[bus_map_merge.ng_dif >0, ['coal']] = bus_map_merge['ng_dif']; bus_map_merge  
-    
-    bus_map_merge.loc[bus_map_merge.coal_dif <0, ['ng']] = abs(bus_map_merge['coal_dif']); bus_map_merge
-    bus_map_merge.loc[bus_map_merge.ng_dif <0, ['ng']] = abs(bus_map_merge['ng_dif']); bus_map_merge  
-    
-    makemap_carbon(bus_map_merge,"compare","green","red","Less: tons", "More: tons")
-    
+    bus_map_merge.loc[bus_map_merge.coal_dif >
+                      0, ['coal']] = bus_map_merge['coal_dif']
+    bus_map_merge
+    bus_map_merge.loc[bus_map_merge.ng_dif >
+                      0, ['coal']] = bus_map_merge['ng_dif']
+    bus_map_merge
+
+    bus_map_merge.loc[bus_map_merge.coal_dif < 0,
+                      ['ng']] = abs(bus_map_merge['coal_dif'])
+    bus_map_merge
+    bus_map_merge.loc[bus_map_merge.ng_dif < 0,
+                      ['ng']] = abs(bus_map_merge['ng_dif'])
+    bus_map_merge
+
+    makemap_carbon(bus_map_merge, "compare", "green",
+                   "red", "Less: tons", "More: tons")
+
     return bus_map_merge
