0c73ef5e8ebbf58aae0b967670614b0cea96a279
dmuldrew <dmuldrew@gmail.com>
2020-01-31 17:23:17 -0800

    test: update test parameters

--- postreise/plot/upload_to_blobs/tests/test_blob_upload.py
+++ postreise/plot/upload_to_blobs/tests/test_blob_upload.py
@@ -1,10 +1,11 @@
 from postreise.plot.upload_to_blobs.blob_upload import BlobUtil
-import urllib
 
 
 def test_blob_upload():
-    connection_string = "DefaultEndpointsProtocol=https;AccountName=cleanenergyteststore;AccountKey=E0kD1PJ3eP54t30EbnSGTUPbhZacbsQ1CBpyx9KJDMgwhnXeseZ0rgcVxSsTlHgUQto9ugM6sgFl9APg0Dy9Pg==;EndpointSuffix=core.windows.net"
+    conn_str  = "DefaultEndpointsProtocol=https;AccountName=cleanenergyteststore;AccountKey=E0kD1PJ3eP54t30EbnSGTUPbhZacbsQ1CBpyx9KJDMgwhnXeseZ0rgcVxSsTlHgUQto9ugM6sgFl9APg0Dy9Pg==;EndpointSuffix=core.windows.net"
+    blob_store = BlobUtil(conn_str , 'test-blob')
 
-    blob_store = BlobUtil(connection_string, 'test-blob')
-    blob_store.upload_figure_as_blob(200, 'TestFigure.png', 'TestFigure.png')
-    
+    scenario_id = '88'
+    fig_path = './WesternInfeasibilities.png'
+    file_name = 'WesternInfeasibilities.png'
+    blob_store.upload_figure_as_blob(scenario_id, fig_path, file_name)
