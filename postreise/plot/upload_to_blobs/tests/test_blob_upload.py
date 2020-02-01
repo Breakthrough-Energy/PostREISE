c87d621c1198a5d5396a481dfd871bf43765d5aa
dmuldrew <dmuldrew@gmail.com>
2020-01-31 17:25:34 -0800

    fix: filenames

--- postreise/plot/upload_to_blobs/tests/test_blob_upload.py
+++ postreise/plot/upload_to_blobs/tests/test_blob_upload.py
@@ -5,7 +5,7 @@ def test_blob_upload():
     conn_str  = "DefaultEndpointsProtocol=https;AccountName=cleanenergyteststore;AccountKey=E0kD1PJ3eP54t30EbnSGTUPbhZacbsQ1CBpyx9KJDMgwhnXeseZ0rgcVxSsTlHgUQto9ugM6sgFl9APg0Dy9Pg==;EndpointSuffix=core.windows.net"
     blob_store = BlobUtil(conn_str , 'test-blob')
 
-    scenario_id = '88'
-    fig_path = './WesternInfeasibilities.png'
-    file_name = 'WesternInfeasibilities.png'
+    scenario_id = '99'
+    fig_path = './TestFigure.png'
+    file_name = 'TestFigure.png'
     blob_store.upload_figure_as_blob(scenario_id, fig_path, file_name)
