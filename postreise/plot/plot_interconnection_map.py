268b8bdcc930c29c52b7b9cbae3e79e6dbfae59a
Vicky Hunt <49412554+victoriahunt@users.noreply.github.com>
2020-08-19 11:40:59 -0700

    Add hover tool tips to Interconnections Map (#140)
    Feat: hover-tool tips and update colors for website. Relates to #131 Postreise: Bokeh web Map Compatibility

--- postreise/plot/plot_interconnection_map.py
+++ postreise/plot/plot_interconnection_map.py
@@ -1,16 +1,17 @@
 from postreise.plot.projection_helpers import project_branch
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, HoverTool
 from bokeh.plotting import figure
 from bokeh.sampledata import us_states
 from postreise.plot import plot_carbon_map
 from bokeh.tile_providers import get_provider, Vendors
 
 
-def map_interconnections(grid, us_states_dat=us_states.data):
-    """Makes map of transmission color coded by interconnection
+def map_interconnections(grid, hvdc_width=1, us_states_dat=us_states.data):
+    """Maps transmission lines color coded by interconnection
 
     :param grid: grid object
     :param dict us_states_dat: us_states data file, imported from bokeh.
+    :param float hvdc_width: adjust width of HVDC lines on map
     :return:  -- map of transmission
     """
     # projection steps for mapping
@@ -31,11 +32,13 @@ def map_interconnections(grid, us_states_dat=us_states.data):
     a, b = plot_carbon_map.get_borders(us_states_dat.copy())
 
     # transmission data sources
+    line_width_const = 0.000225
+
     multi_line_source = ColumnDataSource(
         {
             "xs": branch_west[["from_x", "to_x"]].values.tolist(),
             "ys": branch_west[["from_y", "to_y"]].values.tolist(),
-            "capacity": (branch_west.rateA) * 0.000225 + 0.1,
+            "capacity": branch_west.rateA * line_width_const + 0.1,
         }
     )
 
@@ -43,7 +46,7 @@ def map_interconnections(grid, us_states_dat=us_states.data):
         {
             "xs": branch_east[["from_x", "to_x"]].values.tolist(),
             "ys": branch_east[["from_y", "to_y"]].values.tolist(),
-            "capacity": (branch_east.rateA) * 0.000225 + 0.1,
+            "capacity": branch_east.rateA * line_width_const + 0.1,
         }
     )
 
@@ -51,7 +54,7 @@ def map_interconnections(grid, us_states_dat=us_states.data):
         {
             "xs": branch_tx[["from_x", "to_x"]].values.tolist(),
             "ys": branch_tx[["from_y", "to_y"]].values.tolist(),
-            "capacity": (branch_tx.rateA) * 0.000225 + 0.1,
+            "capacity": branch_tx.rateA * line_width_const + 0.1,
         }
     )
 
@@ -59,72 +62,53 @@ def map_interconnections(grid, us_states_dat=us_states.data):
         {
             "xs": branch_mdc[["from_x", "to_x"]].values.tolist(),
             "ys": branch_mdc[["from_y", "to_y"]].values.tolist(),
-            "capacity": (branch_mdc.Pmax.astype(float)) * 0.000225 + 0.1,
+            "capacity": branch_mdc.Pmax.astype(float) * line_width_const * hvdc_width
+            + 0.1,
+            "cap": branch_mdc.Pmax.astype(float),
         }
     )
 
     # Set up figure
-    tools: str = "pan,wheel_zoom,reset,hover,save"
+    tools: str = "pan, wheel_zoom, reset, save"
 
     p = figure(
-        title="Interconnections",
         tools=tools,
         x_axis_location=None,
         y_axis_location=None,
         plot_width=800,
         plot_height=800,
         output_backend="webgl",
+        sizing_mode="stretch_both",
+        match_aspect=True,
     )
 
-    # for legend
-    p.multi_line(
-        [-1.084288e07, -1.084288e07],
-        [4.639031e06, 4.639031e06],
-        color="blue",
-        line_width=5,
-        legend="Western",
-    )
-    p.multi_line(
-        [-1.084288e07, -1.084288e07],
-        [4.639031e06, 4.639031e06],
-        color="red",
-        line_width=5,
-        legend="Eastern",
-    )
-    p.multi_line(
-        [-1.084288e07, -1.084288e07],
-        [4.639031e06, 4.639031e06],
-        color="purple",
-        line_width=5,
-        legend="ERCOT",
-    )
-    p.multi_line(
-        [-1.084288e07, -1.084288e07],
-        [4.639031e06, 4.639031e06],
-        color="green",
-        line_width=5,
-        legend="HVDC",
-    )
-    p.square(
-        x=[-1.084288e07], y=[4.639031e06], size=170, fill_color="white", color="white"
-    )
+    # for legend, hidden lines
+    leg_clr = ["#d8428d", "#0c84e2", "#6D3376", "#012f56"]
+    leg_lab = ["Western", "Eastern", "ERCOT", "HVDC"]
+    leg_xs = [-1.084288e07] * 4
+    leg_ys = [4.639031e06] * 4
+
+    for (colr, leg, x, y) in zip(leg_clr, leg_lab, leg_xs, leg_ys):
+        p.line(x, y, color=colr, width=5, legend=leg)
+
+    # background tiles
     p.add_tile(get_provider(Vendors.CARTODBPOSITRON))
 
     # state borders
-    p.patches(a, b, fill_alpha=0.0, line_color="black", line_width=2)
+    p.patches(a, b, fill_alpha=0.0, line_color="#808184", line_width=2)
+
     # branches
-    p.multi_line(
-        "xs", "ys", color="blue", line_width="capacity", source=multi_line_source
-    )
-    p.multi_line(
-        "xs", "ys", color="red", line_width="capacity", source=multi_line_source2
-    )
-    p.multi_line(
-        "xs", "ys", color="purple", line_width="capacity", source=multi_line_source3
-    )
-    p.multi_line(
-        "xs", "ys", color="green", line_width="capacity", source=multi_line_source4
+    source_list = [multi_line_source, multi_line_source2, multi_line_source3]
+
+    for (colr, source) in zip(leg_clr[0:3], source_list):
+        p.multi_line("xs", "ys", color=colr, line_width="capacity", source=source)
+
+    lines = p.multi_line(
+        "xs", "ys", color="#012f56", line_width="capacity", source=multi_line_source4
     )
     p.legend.location = "bottom_right"
+
+    hover = HoverTool(tooltips=[("HVDC capacity MW", "@cap"),], renderers=[lines])
+    p.add_tools(hover)
 
     return p
