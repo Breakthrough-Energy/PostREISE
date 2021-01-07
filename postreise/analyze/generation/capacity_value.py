bcd3994c59c6ebb430d5ca65bb57af6ebc0bb3c8
BainanXia <bainan.xia@breakthroughenergy.org>
2021-01-06 19:09:05 -0600

    feat: add functions to get capacity of resources and storage

--- postreise/analyze/generation/capacity_value.py
+++ postreise/analyze/generation/capacity_value.py
@@ -3,6 +3,7 @@ from postreise.analyze.check import (
     _check_resources_are_in_grid_and_format,
     _check_scenario_is_in_analyze_state,
 )
+from postreise.analyze.helpers import get_plant_id_by_resources, get_storage_id
 
 
 def calculate_NLDC(scenario, resources, hours=100):
@@ -57,3 +58,37 @@ def calculate_net_load_peak(scenario, resources, hours=100):
     net_demand = total_demand - resource_generation
     top_hours = net_demand.sort_values(ascending=False).head(hours).index
     return resource_generation[top_hours].mean()
+
+
+def get_capacity_by_resources(scenario, area, resources, area_type=None):
+    """Get total capacity value of certain resources in the specific area of a
+    scenario.
+
+    :param powersimdata.scenario.scenario.Scenario scenario: scenario instance
+    :param str area: one of: *loadzone*, *state*, *state abbreviation*,
+        *interconnect*, *'all'*
+    :param str/list resources: one or a list of resources
+    :param str area_type: one of: *'loadzone'*, *'state'*,
+        *'state_abbr'*, *'interconnect'*
+    :return: (*pandas.Series*) -- index: resources, column: total capacity values
+    """
+    plant_id = get_plant_id_by_resources(scenario, area, resources, area_type=area_type)
+    grid = scenario.state.get_grid()
+
+    return grid.plant.loc[plant_id].groupby("type")["Pmax"].sum()
+
+
+def get_storage_capacity(scenario, area, area_type=None):
+    """Get total storage capacity value in the specific area of a scenario.
+
+    :param powersimdata.scenario.scenario.Scenario scenario: scenario instance
+    :param str area: one of: *loadzone*, *state*, *state abbreviation*,
+        *interconnect*, *'all'*
+    :param str area_type: one of: *'loadzone'*, *'state'*,
+        *'state_abbr'*, *'interconnect'*
+    :return: (*float*) -- total storage capacity value
+    """
+    grid = scenario.state.get_grid()
+    storage_id = get_storage_id(scenario, area, area_type)
+
+    return sum(grid.storage["gen"].loc[storage_id].Pmax.values)
