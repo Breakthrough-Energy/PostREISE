527f8d08e865ef22cff611766b63e0023489c1a4
Merrielle Ondreicka <merrielle@users.noreply.github.com>
2021-01-14 16:45:28 -0800

    feat: add option to create bokeh figures for the website (#203)

--- postreise/plot/plot_lmp_map.py
+++ postreise/plot/plot_lmp_map.py
@@ -12,7 +12,7 @@ from bokeh.tile_providers import Vendors, get_provider
 from postreise.plot.projection_helpers import project_borders, project_bus
 
 
-def map_lmp(s_grid, lmp, us_states_dat=None, lmp_min=20, lmp_max=45):
+def map_lmp(s_grid, lmp, us_states_dat=None, lmp_min=20, lmp_max=45, is_website=False):
     """Plots average LMP by color coding buses
 
     :param powersimdata.input.grid.Grid s_grid: scenario grid
@@ -20,6 +20,7 @@ def map_lmp(s_grid, lmp, us_states_dat=None, lmp_min=20, lmp_max=45):
     :param dict us_states_dat: if None default to us_states data file, imported from bokeh
     :param inf/float lmp_min: minimum LMP to clamp plot range to.
     :param inf/float lmp_max: maximum LMP to clamp plot range to.
+    :param bool is_website: changes text/legend formatting to look better on the website
     :return: (*bokeh.models.layout.Row*) bokeh map visual in row layout
     """
     if us_states_dat is None:
@@ -27,7 +28,9 @@ def map_lmp(s_grid, lmp, us_states_dat=None, lmp_min=20, lmp_max=45):
 
     bus = project_bus(s_grid.bus)
     bus_segments = _construct_bus_data(bus, lmp, lmp_min, lmp_max)
-    return _construct_shadowprice_visuals(bus_segments, us_states_dat, lmp_min, lmp_max)
+    return _construct_shadowprice_visuals(
+        bus_segments, us_states_dat, lmp_min, lmp_max, is_website
+    )
 
 
 def _construct_bus_data(bus_map, lmp, lmp_min, lmp_max):
@@ -75,7 +78,9 @@ def group_lat_lon(bus_map):
     return bus_map2
 
 
-def _construct_shadowprice_visuals(bus_segments, us_states_dat, lmp_min, lmp_max):
+def _construct_shadowprice_visuals(
+    bus_segments, us_states_dat, lmp_min, lmp_max, is_website=False
+):
     """Uses bokeh to plot formatted data. Make map showing lmp using color.
 
     :param list(pandas.DataFrame) bus_segments: bus data split by lmp
@@ -83,6 +88,7 @@ def _construct_shadowprice_visuals(bus_segments, us_states_dat, lmp_min, lmp_max
     :param str file_name: name for output png file
     :param inf/float lmp_min: minimum LMP to clamp plot range to.
     :param inf/float lmp_max: maximum LMP to clamp plot range to.
+    :param bool is_website: changes text/legend formatting to look better on the website
     :return: (*bokeh.models.layout.Row*) bokeh map visual in row layout
     """
 
@@ -126,15 +132,16 @@ def _construct_shadowprice_visuals(bus_segments, us_states_dat, lmp_min, lmp_max
     )
     p.add_tools(hover)
     # Add legend
-    bus_legend = _construct_bus_legend(lmp_min, lmp_max)
-    return row(bus_legend, p)
+    bus_legend = _construct_bus_legend(lmp_min, lmp_max, is_website)
+    return row(bus_legend, p, sizing_mode="stretch_both")
 
 
-def _construct_bus_legend(lmp_min, lmp_max):
+def _construct_bus_legend(lmp_min, lmp_max, is_website=False):
     """Constructs the legend for lmp at each bus
 
     :param inf/float lmp_min: minimum LMP to clamp plot range to.
     :param inf/float lmp_max: maximum LMP to clamp plot range to.
+    :param bool is_website: changes text/legend formatting to look better on the website
     :return: (bokeh.plotting.figure) the legend showing lmp for each bus
     """
     x_range = [""]
@@ -146,10 +153,12 @@ def _construct_bus_legend(lmp_min, lmp_max):
     p = figure(
         x_range=x_range,
         plot_height=800,
-        plot_width=110,
+        plot_width=50 if is_website else 110,
         toolbar_location=None,
         tools="",
         output_backend="webgl",
+        sizing_mode="stretch_height",
+        match_aspect=True,
     )
     p.vbar_stack(
         list(bars.keys())[1:],
@@ -164,10 +173,27 @@ def _construct_bus_legend(lmp_min, lmp_max):
     p.xgrid.grid_line_color = None
     p.axis.minor_tick_line_color = None
     p.outline_line_color = None
-    p.yaxis.ticker = list(labels.keys())
+    ticker_vals = list(labels.keys())
+    # Label colorbar in increments of 5 for website
+    p.yaxis.ticker = ticker_vals[0::5] if is_website else ticker_vals
     p.yaxis.major_label_overrides = labels
 
-    p.text(x=[-0.05], y=[bar_len_sum * 1.01], text=[" $/MWh"], text_font_size="12pt")
+    if is_website:
+        # Prevents $/MWh text from being cut off
+        p.rect(
+            x=0,
+            y=bar_len_sum + 7,
+            width=1,
+            height=7,
+            alpha=0,
+        )
+    p.text(
+        x=[0.05],
+        y=[bar_len_sum * 1.01],
+        text=["$/\nMWh" if is_website else " $/MWh"],
+        text_font_size="7pt" if is_website else "12pt",
+        level="overlay",
+    )
 
     return p
 
@@ -197,9 +223,10 @@ def _get_bus_legend_bars_and_labels(x_range, lmp_min, lmp_max):
             # Min bar length of 1 to ensure small bars are visible
             bar_length = max(1, round(lmp_diff, 1))
 
-        labels[round(bar_length_sum, -1)] = str(
+        label_num = int(
             round(((lmp_split_points[i] - 1) * (lmp_max - lmp_min) / 256 + lmp_min), 0)
         )
+        labels[round(bar_length_sum, -1)] = str(label_num)
         if i != len(lmp_split_points) - 1:
             bars[str(i)] = [bar_length]
             bar_length_sum += bar_length
