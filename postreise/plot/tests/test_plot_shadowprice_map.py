302a4da285a82956628a319df644342b79e1b296
Ben RdO <ben.rouilledorfeuil@breakthroughenergy.org>
2021-01-19 15:06:26 -0800

    refactor: modify tests to reflect changes of function call

--- postreise/plot/tests/test_plot_shadowprice_map.py
+++ postreise/plot/tests/test_plot_shadowprice_map.py
@@ -151,43 +151,35 @@ def test_construct_branch_data():
 
 def test_get_bus_legend_bars_and_labels():
     mock_split_points = [-1, 1, 2, 3, 4]
-    bars, bar_length_sum, labels = _get_bus_legend_bars_and_labels(
-        mock_split_points, [""]
-    )
+    bars, bar_length_sum, labels = _get_bus_legend_bars_and_labels(mock_split_points)
 
-    assert bars == {"x_range": [""], "0": [2], "1": [1], "2": [1], "3": [1]}
+    assert bars == {"0": [2], "1": [1], "2": [1], "3": [1]}
     assert bar_length_sum == 5
     assert labels == {0: "-1", 2: "1", 3: "2", 4: "3", 5: "4"}
 
 
 def test_get_bus_legend_bars_and_labels_with_rounding():
     mock_split_points = [-1, 1, 1.9999, 3, 4]
-    bars, bar_length_sum, labels = _get_bus_legend_bars_and_labels(
-        mock_split_points, [""]
-    )
+    bars, bar_length_sum, labels = _get_bus_legend_bars_and_labels(mock_split_points)
 
-    assert bars == {"x_range": [""], "0": [2], "1": [1], "2": [1.0001], "3": [1]}
+    assert bars == {"0": [2], "1": [1], "2": [1.0001], "3": [1]}
     assert bar_length_sum == 5.0001
     assert labels == {0: "-1", 2: "1", 3: "1.9999", 4.0001: "3", 5.0001: "4"}
 
 
 def test_get_bus_legend_bars_and_labels_clamps_large_numbers():
     mock_split_points = [-500, 100, 200, 300, 400]
-    bars, bar_length_sum, labels = _get_bus_legend_bars_and_labels(
-        mock_split_points, [""]
-    )
+    bars, bar_length_sum, labels = _get_bus_legend_bars_and_labels(mock_split_points)
 
-    assert bars == {"x_range": [""], "0": [5], "1": [5], "2": [5], "3": [5]}
+    assert bars == {"0": [5], "1": [5], "2": [5], "3": [5]}
     assert bar_length_sum == 20
     assert labels == {0: "-500", 5: "100", 10: "200", 15: "300", 20: "400"}
 
 
 def test_get_bus_legend_bars_and_labels_min_bar_len_is_one():
     mock_split_points = [-0.9, 0.1, 0.2, 0.3, 0.4]
-    bars, bar_length_sum, labels = _get_bus_legend_bars_and_labels(
-        mock_split_points, [""]
-    )
+    bars, bar_length_sum, labels = _get_bus_legend_bars_and_labels(mock_split_points)
 
-    assert bars == {"x_range": [""], "0": [1], "1": [1], "2": [1], "3": [1]}
+    assert bars == {"0": [1], "1": [1], "2": [1], "3": [1]}
     assert bar_length_sum == 4.0
     assert labels == {0: "-0.9", 1: "0.1", 2: "0.2", 3: "0.3", 4: "0.4"}
