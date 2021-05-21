f4349e2b5949c631f21e69d64bf51838e5e693ec
Daniel Olsen <daniel.olsen@breakthroughenergy.org>
2021-05-21 10:00:24 -0700

    refactor: rewrite map_plant_capacity function for flexibility

--- postreise/plot/plot_capacity_map.py
+++ postreise/plot/plot_capacity_map.py
@@ -4,113 +4,188 @@
 import pandas as pd
 from bokeh.models import ColumnDataSource, HoverTool
 from bokeh.plotting import figure
-from bokeh.tile_providers import Vendors, get_provider
+from matplotlib import colors as mcolors
+from powersimdata import Scenario
 
-from postreise.analyze.check import _check_scenario_is_in_analyze_state
-from postreise.plot.colors import be_green
-from postreise.plot.plot_states import get_state_borders
-from postreise.plot.projection_helpers import project_borders, project_bus
+from postreise.plot.plot_states import plot_states
+from postreise.plot.projection_helpers import project_bus
 
 
-def map_plant_capacity(scenario, us_states_dat=None, size_factor=1):
-    """Makes map of renewables from change table 'new plants'. Size/area
-    indicates capacity.
+def map_plant_capacity(
+    scenario,
+    resources,
+    figsize=(1400, 800),
+    x_range=None,
+    y_range=None,
+    disaggregation=None,
+    plot_states_kwargs=None,
+    min_capacity=1,
+    size_factor=1,
+    alpha=0.5,
+    legend_font_size=12,
+    legend_location="bottom_right",
+):
+    """Makes map of plant capacities, optionally disaggregated by new/existing. Area
+    is proportional to capacity.
 
     :param powersimdata.scenario.scenario.Scenario scenario: scenario instance.
-    :param dict us_states_dat: dictionary of state border lats/lons. If None, get
-        from :func:`postreise.plot.plot_states.get_state_borders`.
+    :param iterable resources: which types of resources to plot.
+    :param tuple figsize: size of the bokeh figure (in pixels).
+    :param tuple(float, float) x_range: x range to zoom plot to (EPSG:3857).
+    :param tuple(float, float) y_range: y range to zoom plot to (EPSG:3857).
+    :param str disaggregation: method used to disaggregate plants:
+        if "new_vs_existing_plants": separates plants into added vs. existing.
+        if None, no disaggregation.
+    :param dict plot_states_kwargs: keyword arguments to pass to
+        :func:`postreise.plot.plot_states.plot_states`.
+    :param float/int min_capacity: minimum bus capacity (MW) for markers to be plotted.
     :param float/int size_factor: scale size of glyphs.
+    :param float/int alpha: opacity of circles (between 0 and 1).
+    :param int/float legend_font_size: font size for legend.
+    :param str legend_location: location for legend.
+    :raises TypeError: if scenario is not a Scenario object.
+    :return: (*bokeh.plotting.figure.Figure*) -- Bokeh map plot of color-coded upgrades.
     """
 
-    _check_scenario_is_in_analyze_state(scenario)
+    if not isinstance(scenario, Scenario):
+        raise TypeError(f"scenario must be a {Scenario} object")
 
-    # prepare data from the change table to select new plants
-    ct = scenario.state.get_ct()
-    # check that there are new plants, check scenario is in analyze state
-    if "new_plant" not in ct.keys():
-        raise ValueError(
-            "There are no new plants added in the selected scenario. Please choose a different scenario."
-        )
-    if us_states_dat is None:
-        us_states_dat = get_state_borders()
-
-    newplant = pd.DataFrame(ct["new_plant"])
-    newplant = newplant.set_index("bus_id")
-    newplant = newplant[newplant.Pmax > 0]
-
-    # merge with bus info to get coordinates
-    gridscen = scenario.state.get_grid()
-    bus_of_interest = gridscen.bus.loc[list(set(newplant.index))]
-    bus_capacity = bus_of_interest.merge(newplant, right_index=True, left_index=True)
-
-    bus_map = project_bus(bus_capacity)
-    bus_map1 = bus_map.loc[bus_map.Pmax > 1]
-    rar_df = bus_map1.loc[(bus_map1.type_y == "solar") | (bus_map1.type_y == "wind")]
-
-    # group by coordinates
-    rar_df = rar_df.groupby(["lat", "lon"]).agg(
-        {"Pmax": "sum", "x": "mean", "y": "mean"}
-    )
-
-    a, b = project_borders(us_states_dat)
-
-    rar_source = ColumnDataSource(
-        {
-            "x": rar_df["x"],
-            "y": rar_df["y"],
-            "capacity": (rar_df["Pmax"] * size_factor) ** 0.5,
-            "capacitymw": rar_df["Pmax"],
-        }
-    )
-
-    tools: str = "pan,wheel_zoom,reset,save"
     p = figure(
-        tools=tools,
+        tools="pan,wheel_zoom,reset,save",
         x_axis_location=None,
         y_axis_location=None,
-        plot_width=800,
-        plot_height=800,
+        plot_width=figsize[0],
+        plot_height=figsize[1],
         output_backend="webgl",
-        sizing_mode="stretch_both",
+        sizing_mode="scale_both",
         match_aspect=True,
+        x_range=x_range,
+        y_range=y_range,
     )
-
-    # for legend, plot behind tiles
-    p.circle(
-        -8.1e6,
-        5.2e6,
-        fill_color=be_green,
-        color=be_green,
-        alpha=0.5,
-        size=50,
-        legend_label="Renewable Capacity Added",
-    )
-
-    p.add_tile(get_provider(Vendors.CARTODBPOSITRON_RETINA))
 
     # state borders
-    p.patches(a, b, fill_alpha=0.0, line_color="gray", line_width=2)
+    default_plot_states_kwargs = {
+        "line_color": "gray",
+        "line_width": 2,
+        "fill_alpha": 0,
+        "background_map": True,
+    }
+    if plot_states_kwargs is not None:
+        all_plot_states_kwargs = {**default_plot_states_kwargs, **plot_states_kwargs}
+    else:
+        all_plot_states_kwargs = default_plot_states_kwargs
+    plot_states(bokeh_figure=p, **all_plot_states_kwargs)
+
+    add_plant_capacity(
+        p,
+        scenario,
+        resources,
+        disaggregation,
+        min_capacity,
+        size_factor,
+        alpha,
+    )
+
+    p.legend.label_text_font_size = f"{legend_font_size}pt"
+    p.legend.location = legend_location
+
+    return p
+
+
+def add_plant_capacity(
+    bokeh_figure,
+    scenario,
+    resources,
+    disaggregation=None,
+    min_capacity=1,
+    size_factor=1,
+    alpha=0.5,
+):
+    """Adds renewables capacity to a plot.
+
+    :param bokeh.plotting.figure.Figure bokeh_figure: figure to plot capacities onto.
+    :param powersimdata.scenario.scenario.Scenario scenario: scenario instance.
+    :param iterable resources: which types of resources to plot.
+    :param tuple figsize: size of the bokeh figure (in pixels).
+    :param tuple(float, float) x_range: x range to zoom plot to (EPSG:3857).
+    :param tuple(float, float) y_range: y range to zoom plot to (EPSG:3857).
+    :param str disaggregation: method used to disaggregate plants:
+        if "new_vs_existing_plants": separates plants into added vs. existing.
+        if None, no disaggregation.
+    :param dict plot_states_kwargs: keyword arguments to pass to
+        :func:`postreise.plot.plot_states.plot_states`.
+    :param float/int min_capacity: minimum bus capacity (MW) for markers to be plotted.
+    :param float/int size_factor: scale size of glyphs.
+    :param float/int alpha: opacity of circles (between 0 and 1).
+    :raises ValueError: if ``disaggregation`` is not 'new_vs_existing_plants' or None.
+    :return: (*bokeh.plotting.figure.Figure*) -- Bokeh map plot of color-coded upgrades.
+    """
+    ct = scenario.get_ct()
+    grid = scenario.get_grid()
+    grid.plant["lat"] = grid.plant["lat"].round(3)
+    grid.plant["lon"] = grid.plant["lon"].round(3)
+    grid.plant = project_bus(grid.plant.query("type in @resources"))
+    type_colors = grid.model_immutables.plants["type2color"]
+
+    xy_capacity = {}
+    if disaggregation is None:
+        grouped_capacities = grid.plant.groupby(["x", "y", "type"]).sum().Pmax
+        grouped_capacities = grouped_capacities.reset_index()
+        xy_capacity["all"] = grouped_capacities.query("Pmax > 0")
+    elif disaggregation == "new_vs_existing_plants":
+        if "new_plant" in ct.keys():
+            num_new_plants = len(ct["new_plant"])
+            scaled_plants = grid.plant.iloc[:-num_new_plants]
+            new_plants = grid.plant.iloc[-num_new_plants:]
+            grouped_new_capacities = new_plants.groupby(["x", "y", "type"]).sum().Pmax
+            grouped_new_capacities = grouped_new_capacities.reset_index()
+            xy_capacity["new"] = grouped_new_capacities.query("Pmax > 0")
+        else:
+            scaled_plants = grid.plant
+            new_plants = pd.DataFrame(columns=grid.plant.columns)
+            xy_capacity["new"] = pd.DataFrame(columns=["x", "y", "type", "Pmax"])
+        grouped_capacities = scaled_plants.groupby(["x", "y", "type"]).sum().Pmax
+        grouped_capacities = grouped_capacities.reset_index()
+        xy_capacity["existing"] = grouped_capacities.query("Pmax > 0")
+    else:
+        raise ValueError(f"Unknown disaggregation method: {disaggregation}")
 
     # capacity circles
-    circle = p.circle(
-        "x",
-        "y",
-        fill_color=be_green,
-        color=be_green,
-        alpha=0.8,
-        size="capacity",
-        source=rar_source,
-    )
-    p.legend.label_text_font_size = "12pt"
-    p.legend.location = "bottom_right"
+    renderers = []
+    for tranche, plants in xy_capacity.items():
+        for resource in sorted(resources):
+            if disaggregation is None:
+                legend_label = f"{resource} capacity"
+            elif disaggregation == "new_vs_existing_plants":
+                legend_label = f"{resource} capacity of {tranche} plants"
+            if resource not in plants.type.unique():
+                print(f"no {resource} plants for grouping: {tranche}")
+                continue
+            matching_plants = plants.query("type == @resource")
+            data = {
+                "x": matching_plants["x"],
+                "y": matching_plants["y"],
+                "capacity": matching_plants["Pmax"],
+                "radius": matching_plants["Pmax"] ** 0.5 * size_factor,
+            }
+            circle = bokeh_figure.circle(
+                "x",
+                "y",
+                color=mcolors.to_hex(type_colors[resource]),
+                alpha=0.8,
+                size="radius",
+                source=ColumnDataSource(data),
+                legend_label=legend_label,
+            )
+            renderers.append(circle)
 
     hover = HoverTool(
         tooltips=[
             ("Capacity (MW)", "@capacitymw"),
         ],
-        renderers=[circle],
+        renderers=renderers,
     )
 
-    p.add_tools(hover)
+    bokeh_figure.add_tools(hover)
 
-    return p
+    return bokeh_figure
