38a305415293aa8177c6b7f0710874714d490e3e
Ben RdO <ben.rouilledorfeuil@breakthroughenergy.org>
2021-06-09 16:07:04 -0700

    refactor: use state borders layer in lmp map

--- postreise/plot/plot_lmp_map.py
+++ postreise/plot/plot_lmp_map.py
@@ -9,11 +9,12 @@ from bokeh.models import (
     LinearColorMapper,
 )
 from bokeh.palettes import Turbo256
-from bokeh.plotting import figure
 from matplotlib.colors import BoundaryNorm
 
 from postreise.analyze.check import _check_scenario_is_in_analyze_state
-from postreise.plot.plot_states import plot_states
+from postreise.plot.canvas import create_map_canvas
+from postreise.plot.check import _check_func_kwargs
+from postreise.plot.plot_states import add_state_borders
 from postreise.plot.projection_helpers import project_bus
 
 
@@ -25,7 +26,7 @@ def map_lmp(
     num_ticks=6,
     figsize=(1400, 800),
     scale_factor=1,
-    background_map=False,
+    state_borders_kwargs=None,
 ):
     """Plot average LMP at bus level.
 
@@ -36,21 +37,19 @@ def map_lmp(
     :param int num_ticks: number of ticks to display on the color bar.
     :param tuple figsize: size of the bokeh figure (in pixels).
     :param int/float scale_factor: scaling factor for size of circles centered on buses.
-    :param bool background_map: whether to plot map tiles behind figure.
+    :param dict state_borders_kwargs: keyword arguments to be passed to
+        :func:`postreise.plot.plot_states.add_state_borders`.
     :return: (*bokeh.plotting.figure*) -- LMP map.
     :raises TypeError:
         if ``coordinate_rounding`` is not ``int``.
         if ``lmp_min`` and ``lmp_max`` are not ``int``.
         if ``num_ticks`` is not ``int``.
-        if ``figsize`` is not a tuple.
         if ``scale_factor`` is not ``int`` or ``float``.
-        if ``background_map`` is not a ``bool``.
     :raises ValueError:
         if ``coordinate_rounding`` is not positive.
         if ``num_ticks`` is negative.
-        if ``lmp_min`` or ``lmp_min`` is negative or ``lmp_min`` >= ``lmp_max``.
+        if ``lmp_min`` or ``lmp_max`` is negative or ``lmp_min`` >= ``lmp_max``.
         if ``scale_factor`` is negative.
-        if both elements of ``figsize`` are not positive.
     """
     _check_scenario_is_in_analyze_state(scenario)
     if not isinstance(coordinate_rounding, int):
@@ -63,23 +62,17 @@ def map_lmp(
         raise TypeError("lmp_max must be a int")
     if lmp_min >= lmp_max:
         raise ValueError("Must have lmp_min < lmp_max")
-    if not isinstance(figsize, tuple):
-        raise TypeError("figsize must be a tuple")
-    if not (len(figsize) == 2 and all(e > 0 for e in figsize)):
-        raise ValueError("both elemets of figsize must be positive")
     if not isinstance(scale_factor, (int, float)):
         raise TypeError("scale_factor must be a int/float")
     if scale_factor < 0:
         raise ValueError("scale_factor must be positive")
-    if not isinstance(background_map, bool):
-        raise TypeError("background_map must be a bool")
 
     grid = scenario.state.get_grid()
     lmp = scenario.state.get_lmp()
     bus_with_lmp = grid.bus.copy()
     bus_with_lmp["lmp"] = lmp.mean()
 
-    return construct_lmp_visuals(
+    return add_lmp(
         bus_with_lmp,
         coordinate_rounding,
         lmp_min,
@@ -87,8 +80,99 @@ def map_lmp(
         num_ticks,
         figsize,
         scale_factor,
-        background_map,
+        state_borders_kwargs,
     )
+
+
+def add_lmp(
+    bus_with_lmp,
+    coordinate_rounding,
+    lmp_min,
+    lmp_max,
+    num_ticks,
+    figsize,
+    scale_factor,
+    state_borders_kwargs,
+):
+    """Add LMP to canvas.
+
+    :param list bus_with_lmp: bus data frame with LMP values.
+    :param int coordinate_rounding: number of digits to round lat/lon for aggregation.
+    :param str file_name: name for output png file.
+    :param int lmp_min: minimum LMP to clamp plot range to.
+    :param int lmp_max: maximum LMP to clamp plot range to.
+    :param int num_ticks: number of ticks to display on the color bar.
+    :param tuple figsize: size of the bokeh figure (in pixels).
+    :param int/float scale_factor: scaling factor for size of circles centered on buses.
+    :param dict state_borders_kwargs: keyword arguments to be passed to
+        :func:`postreise.plot.plot_states.add_state_borders`.
+    :return: (*bokeh.plotting.figure*) -- canvas with LMP..
+    """
+
+    # create canvas
+    canvas = create_map_canvas(figsize=figsize)
+
+    # add state borders
+    default_state_borders_kwargs = {
+        "line_color": "gray",
+        "line_width": 2,
+        "fill_alpha": 0,
+        "background_map": False,
+    }
+    all_state_borders_kwargs = (
+        {**default_state_borders_kwargs, **state_borders_kwargs}
+        if state_borders_kwargs is not None
+        else default_state_borders_kwargs
+    )
+    _check_func_kwargs(
+        add_state_borders, set(all_state_borders_kwargs), "state_borders_kwargs"
+    )
+    canvas = add_state_borders(canvas, **all_state_borders_kwargs)
+
+    # aggregate buses by lat/lon
+    grouped_bus = aggregate_bus_lmp(bus_with_lmp, coordinate_rounding)
+
+    # assign color
+    norm = BoundaryNorm(boundaries=range(lmp_min, lmp_max + 1), ncolors=256, clip=True)
+    grouped_bus["col"] = norm(grouped_bus["lmp"])
+    grouped_bus["col"] = grouped_bus["col"].apply(lambda x: Turbo256[x])
+
+    grouped_bus_info = {
+        "x": grouped_bus["x"],
+        "y": grouped_bus["y"],
+        "col": grouped_bus["col"],
+        "lmp": grouped_bus["lmp"].round(2),
+    }
+
+    circle = canvas.circle(
+        "x",
+        "y",
+        color="col",
+        size=2 * scale_factor,
+        alpha=0.2,
+        source=ColumnDataSource(grouped_bus_info),
+    )
+    hover = HoverTool(
+        tooltips=[
+            ("$/MWh", "@lmp{1.11}"),
+        ],
+        renderers=[circle],
+    )
+    canvas.add_tools(hover)
+
+    # Add color bar
+    cm = LinearColorMapper(palette="Turbo256", low=norm.vmin, high=norm.vmax)
+    cb = ColorBar(
+        color_mapper=cm,
+        ticker=BasicTicker(desired_num_ticks=num_ticks),
+        title="$/MWh",
+        title_standoff=8,
+        orientation="vertical",
+        location=(0, 0),
+    )
+    canvas.add_layout(cb, "left")
+
+    return canvas
 
 
 def aggregate_bus_lmp(bus, coordinate_rounding):
@@ -106,87 +190,3 @@ def aggregate_bus_lmp(bus, coordinate_rounding):
     )
 
     return aggregated
-
-
-def construct_lmp_visuals(
-    bus_with_lmp,
-    coordinate_rounding,
-    lmp_min,
-    lmp_max,
-    num_ticks,
-    figsize,
-    scale_factor,
-    background_map,
-):
-    """Use bokeh to plot formatted data. Make map showing LMP using color.
-
-    :param list bus_with_lmp: bus data frame with LMP values.
-    :param int coordinate_rounding: number of digits to round lat/lon for aggregation.
-    :param str file_name: name for output png file.
-    :param int lmp_min: minimum LMP to clamp plot range to.
-    :param int lmp_max: maximum LMP to clamp plot range to.
-    :param int num_ticks: number of ticks to display on the color bar.
-    :param tuple figsize: size of the bokeh figure (in pixels).
-    :param int/float scale_factor: scaling factor for size of circles centered on buses.
-    :param bool background_map: whether to plot map tiles behind figure.
-    :return: (*bokeh.plotting.figure*) -- bokeh LMP map.
-    """
-
-    p = figure(
-        tools="pan,wheel_zoom,reset,save",
-        x_axis_location=None,
-        y_axis_location=None,
-        plot_width=figsize[0],
-        plot_height=figsize[1],
-        output_backend="webgl",
-        sizing_mode="scale_both",
-        match_aspect=True,
-    )
-    p.xgrid.visible = False
-    p.ygrid.visible = False
-
-    plot_states(bokeh_figure=p, background_map=background_map)
-
-    # aggregate buses by lat/lon
-    grouped_bus = aggregate_bus_lmp(bus_with_lmp, coordinate_rounding)
-
-    # assign color
-    norm = BoundaryNorm(boundaries=range(lmp_min, lmp_max + 1), ncolors=256, clip=True)
-    grouped_bus["col"] = norm(grouped_bus["lmp"])
-    grouped_bus["col"] = grouped_bus["col"].apply(lambda x: Turbo256[x])
-
-    grouped_bus_info = {
-        "x": grouped_bus["x"],
-        "y": grouped_bus["y"],
-        "col": grouped_bus["col"],
-        "lmp": grouped_bus["lmp"].round(2),
-    }
-
-    circle = p.circle(
-        "x",
-        "y",
-        color="col",
-        size=2 * scale_factor,
-        alpha=0.2,
-        source=ColumnDataSource(grouped_bus_info),
-    )
-    hover = HoverTool(
-        tooltips=[
-            ("$/MWh", "@lmp{1.11}"),
-        ],
-        renderers=[circle],
-    )
-    p.add_tools(hover)
-
-    cm = LinearColorMapper(palette="Turbo256", low=norm.vmin, high=norm.vmax)
-    cb = ColorBar(
-        color_mapper=cm,
-        ticker=BasicTicker(desired_num_ticks=num_ticks),
-        title="$/MWh",
-        title_standoff=8,
-        orientation="vertical",
-        location=(0, 0),
-    )
-    p.add_layout(cb, "left")
-
-    return p
