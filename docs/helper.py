af58b514e3e2e1de4bf92d44c4590eb10aeb0a90
Jon Hagg <jon.hagg@breakthroughenergy.org>
2021-06-10 17:00:53 -0700

    chore: add bokeh plots to notebook

--- docs/helper.py
+++ docs/helper.py
@@ -1,4 +1,4 @@
-mpl = {
+mpl_config = {
     "single": {
         "curtailment_eastern": (
             "curtailment_solar_eastern_ts.png",
@@ -24,6 +24,17 @@ mpl = {
         "emission_bar": "emission_bar.png",
         "shortfall_nv": "shortfall_nv.png",
     },
+}
+
+bokeh_config = {
+    "single": {
+        "lmp_usa_map": "lmp_usa_map.html",
+        "utilization_map": "utilization_map.html",
+        "emission_map": "emission_map.html",
+        "pf_snapshot_map": "pf_snapshot_map.png",
+    },
+    "other": {"interconnection_map": "interconnection_map.html"},
+    "comp": {"emission_map_carbon_diff": "emission_map.html"},
 }
 
 
