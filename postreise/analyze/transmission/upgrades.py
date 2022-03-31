cd199d0c35b4c99d06e75133c28127df825b853c
Ben RdO <ben.rouilledorfeuil@breakthroughenergy.org>
2022-03-31 07:59:33 -0700

    chore: remove obsolete module (#336)

--- postreise/analyze/transmission/upgrades.py
+++
@@ -1,6 +0,0 @@
-# These imports are maintained only for backwards-compatibility purposes
-from powersimdata.design.compare.helpers import _reindex_as_necessary  # noqa: F401
-from powersimdata.design.compare.transmission import (  # noqa: F401
-    calculate_branch_difference,
-    calculate_dcline_difference,
-)
