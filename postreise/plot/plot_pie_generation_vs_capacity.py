2151c661caa48092c8a94399351304d53c70245d
Ben RdO <ben.rouilledorfeuil@breakthroughenergy.org>
2022-08-24 09:49:06 -0700

    fix: use list as indexer and instantiate ModelImmutables with interconnect

--- postreise/plot/plot_pie_generation_vs_capacity.py
+++ postreise/plot/plot_pie_generation_vs_capacity.py
@@ -23,10 +23,10 @@ def plot_pie_generation_vs_capacity(
     """Plot any number of scenarios as pie charts with two columns per scenario -
     generation and capacity.
 
-    :param list/str areas: list of area(s), each area is one of *loadzone*, *state*,
-        *state abbreviation*, *interconnect*, *'all'*
-    :param list/str area_types: list of area_type(s), each area_type is one of
-        *'loadzone'*, *'state'*, *'state_abbr'*, *'interconnect'*, defaults to None.
+    :param list/str areas: name of the area to focus on. Could be a loadzone, a state, a
+        country, etc. This will depend on the grid model.
+    :param list/str area_types: area supported by the grid model. See the
+        :func:`powersimdata.network.model.area_to_loadzone` function for more details.
     :param int/list/str scenario_ids: list of scenario id(s), defaults to None.
     :param list/str scenario_names: list of scenario name(s) of same len as scenario
         ids, defaults to None
@@ -50,12 +50,12 @@ def plot_pie_generation_vs_capacity(
         resources with percentage less than the set value will be pooled together,
         defaults to 0.
     :raises ValueError:
-        if length of area_types and areas is different and/or
-        if length of scenario_names and scenario_ids is different and/or
-        if less than two scenario_ids and/or custom_data in total is provided.
+        if length of ``area_types`` and ``areas`` is different.
+        if length of ``scenario_names`` and ``scenario_ids`` is different.
+        if less than two ``scenario_ids`` and/or ``custom_data`` in total is provided.
     :raises TypeError:
-        if resource_labels are provided but not in a dictionary format and/or
-        if resource_colors are provided but not in a dictionary format.
+        if ``resource_labels`` are provided but not in a dict format.
+        if ``resource_colors`` are provided but not in a dict format.
 
     .. note::
         if one wants to plot scenario data and custom data together, custom data MUST be
@@ -69,9 +69,7 @@ def plot_pie_generation_vs_capacity(
     if not area_types:
         area_types = [None] * len(areas)
     if len(areas) != len(area_types):
-        raise ValueError(
-            "ERROR: if area_types are provided, number of area_types must match number of areas"
-        )
+        raise ValueError("area_types and areas must have same length")
 
     if not scenario_ids:
         scenario_ids = []
@@ -80,29 +78,29 @@ def plot_pie_generation_vs_capacity(
     if isinstance(scenario_names, str):
         scenario_names = [scenario_names]
     if scenario_names and len(scenario_names) != len(scenario_ids):
-        raise ValueError(
-            "ERROR: if scenario names are provided, number of scenario names must match number of scenario ids"
-        )
+        raise ValueError("scenario_names and scenario_ids must have same length")
     if not custom_data:
         custom_data = {}
     if len(scenario_ids) + len(custom_data) <= 1:
         raise ValueError(
-            "ERROR: must include at least two scenario ids and/or custom data"
+            "scenario_ids and/or custom_data must have at least two elements"
         )
     if not resource_labels:
         resource_labels = dict()
     if not isinstance(resource_labels, dict):
-        raise TypeError("ERROR: resource_labels should be a dictionary")
+        raise TypeError("resource_labels must be a dict")
     if not resource_colors:
         resource_colors = dict()
     if not isinstance(resource_colors, dict):
-        raise TypeError("ERROR: resource_colors should be a dictionary")
+        raise TypeError("resource_colors must be a dict")
 
     all_loadzone_data = {}
     scenario_data = {}
     for i, sid in enumerate(scenario_ids):
         scenario = Scenario(sid)
-        mi = ModelImmutables(scenario.info["grid_model"])
+        mi = ModelImmutables(
+            scenario.info["grid_model"], interconnect=scenario.info["interconnect"]
+        )
         all_loadzone_data[sid] = {
             "gen": sum_generation_by_type_zone(scenario, time_range, time_zone).rename(
                 columns=mi.zones["id2loadzone"]
@@ -121,11 +119,11 @@ def plot_pie_generation_vs_capacity(
         }
     for area, area_type in zip(areas, area_types):
         for sid in scenario_ids:
-            loadzone_set = area_to_loadzone(
-                scenario_data[sid]["grid_model"], area, area_type
+            loadzone = list(
+                area_to_loadzone(scenario_data[sid]["grid_model"], area, area_type)
             )
             scenario_data[sid]["gen"]["data"][area] = (
-                all_loadzone_data[sid]["gen"][loadzone_set]
+                all_loadzone_data[sid]["gen"][loadzone]
                 .sum(axis=1)
                 .divide(1e6)
                 .astype("float")
@@ -133,7 +131,7 @@ def plot_pie_generation_vs_capacity(
                 .to_dict()
             )
             scenario_data[sid]["cap"]["data"][area] = (
-                all_loadzone_data[sid]["cap"][loadzone_set]
+                all_loadzone_data[sid]["cap"][loadzone]
                 .sum(axis=1)
                 .divide(1e3)
                 .astype("float")
@@ -177,7 +175,6 @@ def _roll_up_small_pie_wedges(resource_data, resource_label, min_percentage):
     """
     resource_list = list(resource_data.keys())
     total_resources = sum(resource_data.values())
-
     small_categories = []
     small_category_value = 0
     small_category_label = ""
